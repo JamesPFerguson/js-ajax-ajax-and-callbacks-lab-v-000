f1ec6e14a764e23f9f128f9ef3d79d88ece2266c
James Ferguson <ncjamesferguson@gmail.com>
2018-06-05 18:27:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function searchRepositories() {
     displayError()
   })
 }
+
+function showRepositories(response) {
+  let repoList = `<ul>${response.items.map(repo => {
+    return (
+      `<li>
+        <h1>${repo.name}</h1>
+        <p>${repo.description}</p>
+        <a href="${repo.html_url}">${repo.html_url}</a>
+        <p>${repo.owner.login}</p>
+        <img src="${repo.owner.avatar_url}" height="32" width="32">
+        <a href="${repo.owner.url}">${repo.owner.url}</a>
+        <a href="#" data-owner="${repo.owner.login}" data-repository="${repo.name}" onClick="showCommits(this)">Show Commits</a>
+      </li>`)}).join('')}
+  </ul>`
+  return repoList
+}
