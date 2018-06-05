8f6417d412aea8a92b915f707b5cace7ac8addf0
James Ferguson <ncjamesferguson@gmail.com>
2018-06-05 18:26:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 $(document).ready(function (){
 });
+
+function searchRepositories() {
+  const searchTerms = document.getElementById('searchTerms').value
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(response) {
+    $("#results").html(showRepositories(response));
+  }).fail(error => {
+    displayError()
+  })
+}
